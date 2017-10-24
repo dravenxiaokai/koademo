57e0daa7ec80cc5935029e142f2f4f8ac524e6b4
dravenxiaokai <jspankai@vip.qq.com>
2017-10-24 11:34:45 +0800

    this.cookies.set

--- koa.js
+++ koa.js
@@ -1,12 +1,17 @@
 const koa = require('koa')
 
 var app = new koa()
+app.keys =['im a new secret','i like koa']
 
 app
     .use(function* () {
         this.body = {
             name: 'aaa'
         }
+        console.log(this.request.url)
+        this.cookies.set('name', 'weibo', {
+            signed: true
+        })
     })
     .listen(3000, 'localhost', () => {
         console.log('starting on port: ', 3000)
